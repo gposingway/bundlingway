05e5aa1ae335b19c811c9818a27ab7e3d4decf81
Leo Botinelly <leonaquitaine@gmail.com>
2025-01-26 12:12:21 -0500

    GPosingway-definitions, the class

---
+++ Client/Model/GposingwayDefinitions.cs
@@ -0,0 +1,18 @@
+﻿namespace Bundlingway.Model
+{
+    public class GposingwayDefinitions
+    {
+        public string version { get; set; }
+        public string gposingwayUrl { get; set; }
+        public List<string> Deprecated { get; set; }
+        public List<OptionalItem> Optional { get; set; }
+
+        public class OptionalItem
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+            public string Mappings { get; set; }
+        }
+    }
+
+}
