ab359482c05fa2e3638a9802f3c3396c7f7fd48a
Leo Botinelly <leonaquitaine@gmail.com>
2025-01-26 12:13:18 -0500

    Maintenance module to centralize operations

---
+++ Client/Utilities/Maintenance.cs
@@ -0,0 +1,23 @@
+﻿using Bundlingway.Model;
+
+namespace Bundlingway.Utilities
+{
+    public static class Maintenance
+    {
+
+        public static void RemoveTempDir()
+        {
+            if (Directory.Exists(Instances.AppDataTempFolder))
+            {
+                Directory.Delete(Instances.AppDataTempFolder, true);
+            }
+        }
+        public static void RemoveCacheDir()
+        {
+            if (Directory.Exists(Instances.AppDataCacheFolder))
+            {
+                Directory.Delete(Instances.AppDataCacheFolder, true);
+            }
+        }
+    }
+}
