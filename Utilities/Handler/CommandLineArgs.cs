dbddfc6eeb35e16b285048340a42a1a0f3b62114
Leo Botinelly <leonaquitaine@gmail.com>
2025-01-30 11:40:52 -0500

    CommandLineArgs handler

---
+++ Utilities/Handler/CommandLineArgs.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Bundlingway.Utilities.Handler
+{
+    public static class CommandLineArgs
+    {
+        public static async Task ProcessAsync(string[] args)
+        {
+            if (args == null || args.Length == 0) return;
+
+            var url = args[0];
+            if (url.StartsWith("gwpreset://open/?", StringComparison.OrdinalIgnoreCase))
+            {
+                url = url["gwpreset://open/?".Length..];
+
+                // Run the installation form in a separate thread
+
+                await UI.NotifyAsync("Installing package", url);
+                await Package.DownloadAndInstall(url);
+            }
+        }
+    }
+}
