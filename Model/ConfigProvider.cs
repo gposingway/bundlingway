6e70388f755dad14b65f611351f17ce05b7903cb
Leo Botinelly <leonaquitaine@gmail.com>
2025-01-30 11:41:04 -0500

    Proper logging

--- Model/ConfigProvider.cs
+++ Model/ConfigProvider.cs
@@ -1,28 +1,15 @@
 ﻿using Bundlingway.Utilities.Extensions;
+using Serilog;
 using System.Reflection;
 
 namespace Bundlingway.Model
 {
     public class ConfigProvider<T> where T : new()
     {
-        public readonly string configFilePath;
-        public readonly string appName;
-        public readonly string appVersion;
-        public readonly string commonAppDataPath;
-
         public T Configuration = new();
 
         public ConfigProvider()
         {
-            appName = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product;
-            appVersion = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
-
-            string commonAppData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            commonAppDataPath = Path.Combine(commonAppData, appName);
-
-            Directory.CreateDirectory(commonAppDataPath);
-            configFilePath = Path.Combine(commonAppDataPath, "config.json");
-
             Load();
         }
 
@@ -30,30 +17,30 @@ namespace Bundlingway.Model
         {
             try
             {
-                Configuration.ToJsonFile(configFilePath);
+                Configuration.ToJsonFile(Instances.ConfigFilePath);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error saving configuration: " + ex.Message);
+                Log.Information("Error saving configuration: " + ex.Message);
             }
         }
 
         public void Load()
         {
-            if (File.Exists(configFilePath))
+            if (File.Exists(Instances.ConfigFilePath))
             {
                 try
                 {
-                    Configuration = Serialization.FromJsonFile<T>(configFilePath);
+                    Configuration = Serialization.FromJsonFile<T>(Instances.ConfigFilePath);
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Error loading configuration: " + ex.Message);
+                    Log.Information("Error loading configuration: " + ex.Message);
                 }
             }
             else
             {
-                Console.WriteLine("Configuration file not found.");
+                Log.Information("Configuration file not found.");
             }
         }
     }
