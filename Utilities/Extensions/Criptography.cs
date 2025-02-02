c481ce228e910aecb7507d7e647414536db8c963
Leo Botinelly <leonaquitaine@gmail.com>
2025-02-02 03:32:30 -0500

    Create Criptography.cs

---
+++ Utilities/Extensions/Criptography.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Bundlingway.Utilities.Extensions
+{
+    public static class Criptography
+    {
+        public static string Sha512(this string input)
+        {
+            using var sha = System.Security.Cryptography.SHA512.Create();
+            byte[] textData = Encoding.UTF8.GetBytes(input);
+            byte[] hash = sha.ComputeHash(textData);
+            return BitConverter.ToString(hash).Replace("-", String.Empty);
+        }
+    }
+}
