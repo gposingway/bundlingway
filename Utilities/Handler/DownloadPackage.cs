b266c56864781615a159fe096c777ea880280be5
Leon Aquitaine <leonaquitaine@gmail.com>
2025-06-20 23:21:28 -0400

    moar

--- Utilities/Handler/DownloadPackage.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Bundlingway.Utilities.Handler
-{
-    // Obsolete: Use DownloadPackage DTO in Model or as part of a Service.
-    [System.Obsolete("Use DownloadPackage DTO in Model or Service instead.")]
-    public class DownloadPackage { }
-}
